db44c8753bfcdcb667a38a836bb6cfd1e1b71dc2
Justin <justinbarrera009@gmail.com>
2021-01-16 14:55:49 -0800

    change the background depending on the time

--- sketch.js
+++ sketch.js
@@ -9,15 +9,18 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-
+var bg="sprites/bg.png"
+var score=0
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImg()
 }
 
 function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+
+
 
 
     ground = new Ground(600,height,1200,20);
@@ -45,18 +48,26 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg)
+
     background(backgroundImg);
+noStroke()
+textSize(35)
+fill(255)
+text("Score: "+score,width-300,50)
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score()
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score()
     log3.display();
 
     box5.display();
@@ -66,7 +77,8 @@ function draw(){
     bird.display();
     platform.display();
     //log6.display();
-    slingshot.display();    
+    slingshot.display();  
+
 }
 
 function mouseDragged(){
@@ -85,4 +97,25 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+}
+
+
+async function getBackgroundImg(){
+
+    var response = await fetch("https://worldtimeapi.org/api/timezone/America/Los_Angeles");
+    var responseJSON = await response.json();
+    console.log(responseJSON)
+
+    var DT = responseJSON.datetime
+    console.log(DT)
+
+    var hour = DT.slice(11,13)
+    console.log(hour)
+    if(hour>=06 && hour<=18){
+        bg="sprites/bg.png"
+    }
+    else{
+        bg="sprites/bg2.jpg"
+    }
+    backgroundImg=loadImage(bg)
 }
